8e08762caa4da864af56f251ddc309e31132572e
R3 <98960638+ELHussein565@users.noreply.github.com>
2022-03-30 09:10:32 +0200

    Add files via upload

---
+++ sub_detector.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#R3-S7S
+url=$1
+if [ -z $url ];then
+echo "Error: Enter website! :)"
+echo "Example -> yahoo.com,google.com Or facebook.com"
+else
+if [ ! -d $url ];then
+mkdir $url
+cd $url
+else
+cd $url
+fi
+fi
+assetfinder $url > subs.txt
+c=0
+for sub in `cat subs.txt`
+do
+if [[ `ping -c 1 $sub 2> /dev/null` ]]
+then
+echo "$sub +++++++++++> pong"
+(( c++ ))
+else
+echo "$sub -----------> error/down"
+ (( c++ ))
+fi
+
+done
+echo "number of subdomain is $c"
