f6d81f3aee3bb978486309b1ba951d3255393dd1
R3-S7s <98960638+ELHussein565@users.noreply.github.com>
2022-03-31 19:21:19 +0200

    Update sub_detector.sh

--- sub_detector.sh
+++ sub_detector.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #R3-S7S
 url=$1
+typeScan=$2
+
 if [ -z $url ];then
 echo "Error: Enter website! :)"
-echo "Example -> yahoo.com,google.com Or facebook.com"
+echo "Example -> bash sub_detector.sh yahoo.com {number of type Scanning}"
+echo "select type Scanner ports {1->quick scan | 2-> frist 1000 ports | 3-> aggressive}"
+exit
 else
 if [ ! -d $url ];then
 mkdir $url
@@ -19,12 +23,27 @@ do
 if [[ `ping -c 1 $sub 2> /dev/null` ]]
 then
 echo "$sub +++++++++++> pong"
+mkdir $sub
+cd $sub
+echo $sub > $sub.txt
+cd ..
+if [ ! -z $typeScan ];then
+if [[ $typeScan == 1 ]];then
+nmap -T4 $sub | tee -a $sub.text
+elif [[ $typeScan ==  2 ]];then
+nmap $sub | tee -a $sub.text
+elif [[ $typeScan == 3 ]];then
+nmap -sV $sub  | tee -a $sub.text
+fi
+else
+echo "you don't select type scanning!!!"
+fi
 (( c++ ))
 else
 echo "$sub -----------> error/down"
  (( c++ ))
 fi
-
 done
 echo "number of subdomain is $c"
-echo "github :https://github.com/ELHussein565"
+
+echo "github: https://github.com/ELHussein565"
